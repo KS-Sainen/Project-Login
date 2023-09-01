38e91b2cd2ca95afc731aa5395377b04a3e24590
Excid <suppavej@hotmail.com>
2023-09-01 22:49:48 +0700

    a

---
+++ s-website/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+      <div className='h-screen flex justify-center items-center'>
+          <h1 className='text-yellow-400 text-5xl font-extrabold'>Tailwind is Awesome!</h1>
+      </div>
+  )
+}
+
+export default App
