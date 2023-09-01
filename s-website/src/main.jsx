d15dc4bd0b9b5627df868dc07642610afbd84bb2
Excid <suppavej@hotmail.com>
2023-09-01 22:52:18 +0700

    t

--- s-website/src/main.jsx
+++ s-website/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="flex w-full min-h-screen justify-center ">
+    <div className="w-96 h-full">
+    <RouterProvider router={router} />
+    </div>
+  </div>
+);
