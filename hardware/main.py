3fca5b19f7413c154b8e6fa2c4856507f2b611ff
KS-Sainen <105944582+KS-Sainen@users.noreply.github.com>
2023-09-03 23:12:58 +0700

    Update main.py
    Building up the foundations for the full UI

--- hardware/main.py
+++ hardware/main.py
@@ -1,10 +1,34 @@
+import guizero
 from time import sleep
+from datetime import datetime
 from picamera import PiCamera
-freq = 60
-buttonPress = False
-# Build UI
+from guizero import App, Text, PushButton, Drawing, Box, Picture
+freq = 30
+margin = 10
+dM = margin * 2
+aH = 480
+aW = 800
+UIfont="Helvetica"
+textB = "#232323"
+textW = "#f9fcff"
+bgW = "#d9d9d9"
+isButtonPress = false
+def dummyFunc():
+    return 0
 
-# Camera Setup
+# Build the UI
+# Masters
+app = App(height=aH,width=aW,title="SADTS",layout="grid",bg="#242528")
+box = Box(app,border=5,width=600,height=400,grid=[0,1],layout="grid",bg=bgW)
+clockBox = Box(app,border=5,width=125,height=80,grid=[0,0],bg=bgW)
+#Components
+timeText = Text(clockBox,size=16,align="middle",font=UIfont,text_color=textB)
+picture = Picture(app, image="input.jpg",grid=[3,0],width=200,height=aH-dM)
+confrText = Text(box,text="Student Name",size=16,grid=[0,0],align="center",font=UIfont,text_color=textB)
+nameText = Text(box,size=14,grid=[0,1],font=UIfont,text_color=textB)
+buttonConfirm = PushButton(box,command=dummyFunc,text="Confirm",grid=[0,2],bg="#59a3f9",font=UIfont,text_color=textW)
+buttonReject = PushButton(box,command=dummyFunc,text="Reject",grid=[2,2],bg="#f95959",font=UIfont,text_color=textW)
+
 camera = PiCamera(resolution=(1280, 720), framerate=30)
 # Set ISO to the desired value
 camera.iso = 100
@@ -17,14 +41,19 @@ g = camera.awb_gains
 camera.awb_mode = 'off'
 camera.awb_gains = g
 
-#Awaiting Login
+#Await Server login
 
+#Main Loop
 while True:
-    # UI Update Loop
-
-    # Picture Taking
-    if buttonPress:
+    #Update UI
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    timeText.text = current_time
+    #Update Name (if there's any)
+    app.display()
+    #Take Image and send it to Face Recognition
+    if isButtonPress:
         camera.capture('input.jpg')
-
-    #Buffer
+        
+    #Update frequency
     sleep(1/freq)
