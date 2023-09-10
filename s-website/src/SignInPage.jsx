5d271d22093cf90f930984477bd47aadfc020826
Excid <suppavej@hotmail.com>
2023-09-10 09:16:17 +0700

    datapao

--- s-website/src/SignInPage.jsx
+++ s-website/src/SignInPage.jsx
@@ -9,13 +9,13 @@ function SignIn() {
             />
             <br/>
             <input
-            type="text"
+            type="password"
             className=" mt-[40px] font-thin tracking-widest w-[445px] px-[20px] py-3 bg-transparent border border-white rounded-xl text-white placeholder-white"
             placeholder="Password"
             />
             <br/>
             <a href="#">
-                <button class="mt-[40px] bg-[#F95959] outline-[#F95959] outline text-white text-[15px] font-bold rounded-lg py-3 px-[190px]">
+                <button class="mt-[50px] bg-[#F95959] outline-[#F95959] outline text-white text-[15px] font-bold rounded-lg py-3 px-[190px]">
                     SIGN IN
                 </button>   
             </a>
