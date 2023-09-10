0b41565682a5d8205b5a0ea93c6fb17076021f70
Excid <suppavej@hotmail.com>
2023-09-10 09:04:17 +0700

    a

--- s-website/src/SignInPage.jsx
+++ s-website/src/SignInPage.jsx
@@ -1,12 +1,24 @@
 function SignIn() {
     return(
         <>
-        <center>
-        <input
-        type="text"
-        class="self-center w-[350px] h-13 p-3 bg-transparent border border-white rounded-xl text-white placeholder-white"
-        placeholder="Email Address"
-        />
+        <center class="pt-[200px]">
+            <input
+            type="text"
+            className="font-thin tracking-widest w-[445px] px-[20px] py-3 bg-transparent border border-white rounded-xl text-white placeholder-white"
+            placeholder="Email Address"
+            />
+            <br/>
+            <input
+            type="text"
+            className=" mt-[40px] font-thin tracking-widest w-[445px] px-[20px] py-3 bg-transparent border border-white rounded-xl text-white placeholder-white"
+            placeholder="Password"
+            />
+            <br/>
+            <a href="#">
+                <button class="mt-[40px] bg-[#F95959] outline-[#F95959] outline text-white text-[15px] font-bold rounded-lg py-3 px-[190px]">
+                    SIGN IN
+                </button>   
+            </a>
         </center>
 
         </>
