5e47278c06bef611d373ab001679805aba2d8083
M <visarut.makjinda@gmail.com>
2023-10-25 20:16:35 +0700

    fix resize problem

--- s-website/src/HomePageComp/IntroPage.jsx
+++ s-website/src/HomePageComp/IntroPage.jsx
@@ -1,12 +1,12 @@
 export default function TextAndSign() {
     return (
         <>
-            <div className="flex text-white">
+            <div className="flex text-white mx-auto ">
                 <div className="flex-1 ml-[100px] tracking-widest">
                     <div className="mb-5 mt-10 font-light text-3xl">Welcome!</div>
                     <div className="mb-10 font-medium text-5xl">Student Arrival<br />& Departure Time<br />System</div>
                     <div className="font-thin text-2xl text-[#A5A6A7] mb-10">Efficiency in education begins<br />with punctuality</div>
-
+                    <div className="flex flex-col space-y-1">
                     <a href="signin">
                         <button className="mt-5 bg-[#59A3F9] outline-[#59A3F9] outline text-white text-[15px] font-bold rounded-lg py-3 px-[190px]">
                             SIGN IN
@@ -18,6 +18,7 @@ export default function TextAndSign() {
                             SIGN UP
                         </button>
                     </a>
+                    </div>
                 </div>
                 <div className="flex-1">
                     <img src="icon2.png" className="w-[550px]" />
