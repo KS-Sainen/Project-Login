c3726fac68704cdaf1393b8d330fb46eeec0ac73
KS-Sainen <105944582+KS-Sainen@users.noreply.github.com>
2023-11-10 20:03:10 +0700

    Update ui-isolated.py
    big UI code revision
    preparing for final assembly
    zzz

--- hardware/ui-isolated.py
+++ hardware/ui-isolated.py
@@ -2,53 +2,70 @@ from time import sleep
 from datetime import datetime
 from guizero import App, Text, PushButton, Drawing, Box, Picture
 freq = 30
-margin = 20
+margin = 15
 dM = margin * 2
 aH = 480
 aW = 800
+clockH = 80
+clockW = 450
+mainH = aH-dM-clockH
+mainW = 400
+pictureW = aW-dM-mainW
 UIfont="Helvetica"
 textB = "#232323"
 textW = "#f9fcff"
 bgW = "#d9d9d9"
+bgC = "#242528"
 isButtonPress = False
 def dummyFunc():
     return 0
 
 # Build the UI
-# Masters
-app = App(height=aH,width=aW,title="SADTS",layout="grid",bg="#242528")
-clockBlock = Box(app,border=0,width=margin,height=margin,grid=[0,0])
-clockBlock.bg = "#242528"
-clockBlock = Box(app,border=0,width=450,height=margin,grid=[1,0])
-clockBlock.bg = "#242528"
-clockBox = Box(app,border=5,width=200,height=80,grid=[1,1],align="left")
-clockBox.bg=bgW
-clockBlock = Box(app,border=0,width=450,height=margin,grid=[1,2])
-clockBlock.bg = "#242528"
-box = Box(app,border=5,width=450,height=300,grid=[1,3],layout="grid")
-box.bg=bgW
+# Masters, [x,y]
+# Grids
+app = App(height=aH,width=aW,title="SADTS",layout="grid",bg=bgC)
+marginR = Box(app,border=0,width=margin,height=margin,grid=[0,0],align="left")
+clockGrid = Box(app,border=0,width=aW,height=clockH,grid=[1,1],layout="grid",align="left")
+mainGrid = Box(app,border=0,width=aW,height=mainH,grid=[1,2],layout="grid",align="top")
 
-#Components
+# Clock + Admin Menu
+clockMargin = Box(clockGrid,border=0,width=margin,height=margin,grid=[0,0],align="left")
+clockMargin.bg = bgC
+clockBox = Box(clockGrid,border=5,width=200,height=clockH,grid=[1,1],align="left")
+clockBox.bg = bgW
 timeText = Text(clockBox,size=24,font=UIfont,color=textB,align="left")
+
+# More Grids
 #picture = Picture(app, image="input.jpg",grid=[3,0],width=200,height=aH-dM)
-confrText = Text(box,text="Student Name:",size=24,grid=[0,0],font=UIfont,color=textB)
-clockBlock = Box(box,border=0,width=200,height=50,grid=[1,0])
-clockBlock.bg = bgW
-# clockBlock = Box(box,border=0,width=225,height=50,grid=[0,1])
-# clockBlock.bg = bgW
-nameText = Text(box,size=24,grid=[0,1],font=UIfont,color=textB,text="Sainen420",align="left")
-clockBlock = Box(box,border=0,width=200,height=margin,grid=[0,2])
-clockBlock.bg = bgW
-buttonConfirm = PushButton(box,command=dummyFunc,text="Confirm",width=10,height=1,padx=5,pady=5,grid=[0,3])
+mainMargin = Box(mainGrid,border=0,width=margin,height=margin,grid=[0,0],align="left")
+mainMargin.bg = bgC
+studentGrid = Box(mainGrid,border=5,width=mainW,height=mainH,grid=[1,1],layout="grid",align="top")
+studentGrid.bg = bgW
+mainMBox = Box(mainGrid,border=0,width=margin,height=mainH,grid=[2,1],layout="grid",align="left")
+pictureGrid = Box(mainGrid,border=0,width=pictureW,height=mainH,grid=[3,1],layout="grid",align="left")
+
+# Two Buttons
+studentMargin = Box(studentGrid,border=0,width=margin,height=margin,grid=[0,0],align="left")
+textCBox = Box(studentGrid,border=0,width=mainW-dM,height=40,grid=[1,1],align="left")
+textConfr = Text(textCBox,size=18,font=UIfont,color=textB,text="Student Name : ")
+fillerBox = Box(studentGrid,border=0,width=mainW-dM,height=margin,grid=[1,2],align="left")
+fillerBox.bg = bgW
+buttonBox = Box(studentGrid,border=0,width=mainW-dM,height=50,grid=[1,3],align="left",layout="grid")
+buttonConfirm = PushButton(buttonBox,command=dummyFunc,text="Confirm",width=10,height=1,padx=5,pady=5,grid=[0,0])
 buttonConfirm.bg = "#59a3f9"
 buttonConfirm.font = UIfont
 buttonConfirm.text_color = textW
 buttonConfirm.text_size	= 16
-buttonReject = PushButton(box,command=dummyFunc,text="Reject",width=10,height=1,padx=5,pady=5,grid=[1,3])
+fillerBox = Box(buttonBox,border=0,width=margin*2,height=50,grid=[1,0],align="left")
+buttonReject = PushButton(buttonBox,command=dummyFunc,text="Reject",width=10,height=1,padx=5,pady=5,grid=[2,0])
 buttonReject.bg = "#f95959"
 buttonReject.font = UIfont
 buttonReject.text_color = textW
 buttonReject.text_size	= 16
+studentGrid.height = mainH - dM
+# Image Display and Overlay
+pictureDisplay = Box(pictureGrid,border=5,width=pictureW-dM,height=mainH-dM,grid=[0,0],align="left")
+pictureDisplay.bg = bgW
 
 #Callbacks
 def updateTime():
@@ -60,4 +77,5 @@ timeText.repeat(1000,updateTime)
 #Await Server login
 
 #Main Loop
+updateTime()
 app.display()
